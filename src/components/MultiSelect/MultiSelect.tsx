374ed952860f206053c87d0909f60d991bc5272b
toqaewa <kath.tokaeva@gmail.com>
2025-06-02 08:10:18 +0300

    feat: multiselect ui

--- src/components/MultiSelect/MultiSelect.tsx
+++ src/components/MultiSelect/MultiSelect.tsx
@@ -1,22 +1,35 @@
 import React from 'react';
 import Select from 'react-select';
+import './MultiSelect.css';
 
 interface MultiSelectProps {
   options: string[];
   selected: string[];
-  onChange: (selected: string[]) => void;
+  onChange: (selected: string[] | null) => void;
+  placeholder?: string;
 }
 
-export const MultiSelect: React.FC<MultiSelectProps> = ({ options, selected, onChange }) => {
+export const MultiSelect: React.FC<MultiSelectProps> = ({ 
+  options, 
+  selected, 
+  onChange, 
+  placeholder = 'Фильтр'
+}) => {
   return (
-    <Select
-      isMulti
-      options={options.map(option => ({ value: option, label: option }))}
-      value={selected.map(s => ({ value: s, label: s }))}
-      onChange={(selectedOptions) => 
-        onChange(selectedOptions.map(option => option.value))
-      }
-      placeholder="Фильтр..."
-    />
+    <div className='multi-select-container'>
+        {selected.length === 0 ? <div className='multi-select-placeholder'>{placeholder}</div> : <div className='multi-select-placeholder-minimized'>{placeholder}</div> }
+        <Select
+            isMulti
+            options={options.map(option => ({ value: option, label: option }))}
+            value={selected.map(s => ({ value: s, label: s }))}
+            onChange={(selectedOptions) => {
+                onChange(selectedOptions ? selectedOptions.map(option => option.value) : null);
+            }}
+            placeholder=''
+            className='multi-select'
+            classNamePrefix='select'
+            noOptionsMessage={() => 'Нет вариантов'}
+        />
+    </div>
   );
 };
