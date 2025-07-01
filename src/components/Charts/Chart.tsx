b69ff2188347691ceacf6e031270ecd665d4e3bc
toqaewa <kath.tokaeva@gmail.com>
2025-07-01 10:25:34 +0300

    feat: chart noData

--- src/components/Charts/Chart.tsx
+++ src/components/Charts/Chart.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React from "react";
 import "../../index.css";
-import "./Chart.css"
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
-import CustomTooltip from './CustomTooltip';
+import "./Chart.css";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+import CustomTooltip from "./CustomTooltip";
 
 interface ChartProps {
   data: { type: string; count?: number; estimate?: number }[];
@@ -13,21 +21,27 @@ interface ChartProps {
 export const Chart: React.FC<ChartProps> = ({ data, name, dataKey }) => (
   <div className="chart-container">
     <h2>{name}</h2>
-    <ResponsiveContainer width="100%" height={400}>
-      <BarChart data={data}>
-        <CartesianGrid horizontal={true} vertical={false} strokeDasharray="3 3" strokeOpacity="0.3" />
-        <XAxis dataKey="type" />
-        <YAxis />
-        <Tooltip 
-          content={<CustomTooltip />}
-          cursor={false}
-        />
-        <Bar 
-          dataKey={dataKey} 
-          radius={[4, 4, 0, 0]}
-          activeBar={{ fillOpacity: 0.8 }}
-        />
-      </BarChart>
-    </ResponsiveContainer>
+    {!data || data.length === 0 ? (
+      <div className="no-data-message">Нет данных для отображения</div>
+    ) : (
+      <ResponsiveContainer width="100%" height={400}>
+        <BarChart data={data}>
+          <CartesianGrid
+            horizontal={true}
+            vertical={false}
+            strokeDasharray="3 3"
+            strokeOpacity="0.3"
+          />
+          <XAxis dataKey="type" />
+          <YAxis />
+          <Tooltip content={<CustomTooltip />} cursor={false} />
+          <Bar
+            dataKey={dataKey}
+            radius={[4, 4, 0, 0]}
+            activeBar={{ fillOpacity: 0.8 }}
+          />
+        </BarChart>
+      </ResponsiveContainer>
+    )}
   </div>
 );
